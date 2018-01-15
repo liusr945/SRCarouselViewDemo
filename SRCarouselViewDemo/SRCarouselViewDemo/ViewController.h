704cc0e59217a71c9f21ef05d080d2325aa68e30
liusr <liusr163@163.com>
2018-01-15 23:19:38 +0800

    创建工程

---
+++ SRCarouselViewDemo/SRCarouselViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SRCarouselViewDemo
+//
+//  Created by liusr on 2018/1/15.
+//  Copyright © 2018年 liusr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SRCarouselViewDemo/SRCarouselViewDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SRCarouselViewDemo
+//
+//  Created by liusr on 2018/1/15.
+//  Copyright © 2018年 liusr. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController () 
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
